608e02df8a22b7808eb72d0af14ae2f6af3a6cfb
dany-rg <71352714+dany-rg@users.noreply.github.com>
2020-09-17 16:13:51 -0400

    Update GatewayChecksum.php

--- src/GatewayChecksum.php
+++ src/GatewayChecksum.php
@@ -11,8 +11,8 @@ namespace RocketGate\Sdk;
 class GatewayChecksum
 {
     public static $checksum = "";
-    public static $baseChecksum = "459c02fd0f0c643db44593cec77de226";
-    public static $versionNo = "P7.4";
+    public static $baseChecksum = "e628f7cc479bbf27e3a64db57e3be874";
+    public static $versionNo = "P7.5";
 
 //////////////////////////////////////////////////////////////////////
 //
@@ -30,7 +30,7 @@ class GatewayChecksum
             md5_file($dirName . "/GatewayCodes.php");
         GatewayChecksum::$checksum = md5($baseString);
         if (GatewayChecksum::$checksum != GatewayChecksum::$baseChecksum) {
-            GatewayChecksum::$versionNo = "P7.4m";
+            GatewayChecksum::$versionNo = "P7.5m";
         }
     }
 }
