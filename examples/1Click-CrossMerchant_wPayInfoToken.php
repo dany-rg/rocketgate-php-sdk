2876fda23f45c5c5e78b186856044db709cf75f4
dany-rg <71352714+dany-rg@users.noreply.github.com>
2020-09-17 16:01:59 -0400

    Update 1Click-CrossMerchant_wPayInfoToken.php

--- examples/1Click-CrossMerchant_wPayInfoToken.php
+++ examples/1Click-CrossMerchant_wPayInfoToken.php
@@ -108,6 +108,7 @@ $service->SetTestMode(TRUE);
 if ($service->PerformPurchase($request, $response)) {
   print "Purchase succeeded\n";
   print "GUID: " . $response->Get(GatewayResponse::TRANSACT_ID()) . "\n";
+  print "Transaction time: " . $response->Get(GatewayResponse::TRANSACTION_TIME()) . "\n";
 
 	
 	//
@@ -149,6 +150,7 @@ if ($service->PerformPurchase($request, $response)) {
 } else {
   print "Purchase failed\n";
   print "GUID: " . $response->Get(GatewayResponse::TRANSACT_ID()) . "\n";
+  print "Transaction time: " . $response->Get(GatewayResponse::TRANSACTION_TIME()) . "\n";
   print "Response Code: " .  $response->Get(GatewayResponse::RESPONSE_CODE()) . "\n";
   print "Reason Code: " .  $response->Get(GatewayResponse::REASON_CODE()) . "\n";
   print "Exception: " .  $response->Get(GatewayResponse::EXCEPTION()) . "\n";
