1f96c7a62f48956303f205092df5a896c17df0d5
dany-rg <71352714+dany-rg@users.noreply.github.com>
2020-09-17 16:12:04 -0400

    Update CancelPromo.php

--- examples/CancelPromo.php
+++ examples/CancelPromo.php
@@ -108,6 +108,7 @@ if ($service->PerformPurchase($request, $response)) {
   print "AVS: " . $response->Get(GatewayResponse::AVS_RESPONSE()) . "\n";
   print "CVV2: " . $response->Get(GatewayResponse::CVV2_CODE()) . "\n";
   print "GUID: " . $response->Get(GatewayResponse::TRANSACT_ID()) . "\n";
+  print "Transaction time: " . $response->Get(GatewayResponse::TRANSACTION_TIME()) . "\n";
   print "Account: " .
 	$response->Get(GatewayResponse::MERCHANT_ACCOUNT()) . "\n";
   print "Scrub: " .
@@ -140,6 +141,7 @@ if ($service->PerformPurchase($request, $response)) {
 } else {
   print "Purchase failed\n";
   print "GUID: " . $response->Get(GatewayResponse::TRANSACT_ID()) . "\n";
+  print "Transaction time: " . $response->Get(GatewayResponse::TRANSACTION_TIME()) . "\n";
   print "Response Code: " .
 	$response->Get(GatewayResponse::RESPONSE_CODE()) . "\n";
   print "Reason Code: " .
